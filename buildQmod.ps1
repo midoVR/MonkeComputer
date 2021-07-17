6e0d2e5760b2cf6dd3c7693d7139b330a67486bd
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-07-17 21:09:24 +0200

    Create Qmod creates proper qmod

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -1,20 +1,3 @@
-# Builds a .zip file for loading with BMBF
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
-
-$buildScript = "$NDKPath/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
-}
-
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
-Compress-Archive -Path  "./extern/libbeatsaber-hook_1_3_3.so",`
-                        "./libs/arm64-v8a/libbeatsaber-hook_1_3_5.so",`
-                        "./libs/arm64-v8a/libcustom-types.so",`
-                        "./libs/arm64-v8a/libmonkecomputer.so",`
-                        "./libs/arm64-v8a/libquest-cosmetic-loader_0_1_4.so",`
-                        "./libs/arm64-v8a/libzip.so",`
-                        "./ExtraFiles/Monitor.package",`
-                        "./cover.png",`
-                        "./mod.json" -DestinationPath "./MonkeComputer.zip" -Update
-
-& copy-item -Force "./MonkeComputer.zip" "./MonkeComputer.qmod"
+& ./build.ps1
+$env:qmodName = "MonkeComputer"
+& ./CreateQmod.ps1
